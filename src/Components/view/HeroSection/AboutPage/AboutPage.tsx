4a9b2c5603412e82bf36206d2fab401081720ab0
M-Hassan1 <hassan81932@gmail.com>
2023-07-05 18:53:35 +0500

    Check Error Amd Solved

--- src/Components/view/HeroSection/AboutPage/AboutPage.tsx
+++ src/Components/view/HeroSection/AboutPage/AboutPage.tsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
   return (
     <div className="flex flex-col mt-32 md:flex-row  ">
       <div className="w-full md:w-1/2">
-      <div className="p-4">
+      <div className="p-4 ">
           <h1 className="text-2xl font-oswald justify-center md:text-3xl font-bold mb-4">About Us</h1>
           <p className="text-sm font-oswald md:text-base lg:text-lg xl:text-xl">
           Moringa powder is a nutrient-dense green powder derived from the leaves of the Moringa oleifera tree. It is rich in vitamins, minerals, antioxidants, and protein, making it a popular superfood and natural supplement with potential health benefits.
